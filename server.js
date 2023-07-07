5f8fbcf37ea91b15843a70905fdf85ab16587c6c
saravana sai <saravanasai002@gmail.com>
2023-07-07 20:28:26 +0530

    checked for 9lkh rows

--- server.js
+++ server.js
@@ -1,13 +1,49 @@
 const express = require("express");
 const db = require("./models");
+const fs = require("fs");
+const PDFDocument = require("pdfkit-table");
 const { Writable, Readable } = require("stream");
 
 const app = express();
 
 app.get("/", async function (req, res) {
+  // init document
+  let doc = new PDFDocument({ margin: 30, size: "A4" });
+  // save document
+  doc.pipe(fs.createWriteStream("./document.pdf"));
+
+  async function createTable(data) {
+    
+    const table = {
+      title: "Title",
+      subtitle: "Subtitle",
+      headers: [
+        { label: "Sno", property: "id", width: 60, renderer: null },
+        { label: "Username", property: "username", width: 150, renderer: null },
+        { label: "Email", property: "email", width: 100, renderer: null },
+      ],
+    
+      datas:data
+
+    
+    };
+    
+    doc.table(table, {
+      prepareHeader: () => doc.font("Helvetica-Bold").fontSize(8),
+      prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
+        doc.font("Helvetica").fontSize(8);
+        indexColumn === 0 && doc.addBackground(rectRow, "blue", 0.15);
+      },
+    });
+   
+    doc.end();
+  }
+
   var page = 0;
   var perPage = 1000;
+  var loadedData = [];
   var totalRecords = await db.User.count();
+  // var totalRecords = 10;
   var totalPages = Math.round(totalRecords / perPage);
   console.log("🚀 ~ file: server.js:18 ~ totalPages:", totalPages);
 
@@ -25,7 +61,16 @@ app.get("/", async function (req, res) {
     },
   });
 
-  datachuck.pipe(res);
+  datachuck.addListener("data", (data) => {
+    loadedData= [...loadedData,...JSON.parse(data)];
+  });
+
+  datachuck.addListener("end", () => {
+    createTable(loadedData);
+    console.log("total records",totalRecords)
+  });
+
+  res.send("okokok");
 });
 
 app.listen(3001, () => {
